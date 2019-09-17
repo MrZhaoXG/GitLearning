8cd781a33652ca0c6d9f84952e413c362ee2e257
xiguan <1030034137@qq.com>
2019-09-17 19:20:33 +0800

    Test Git Manage

--- GitLearning/ViewController.swift
+++ GitLearning/ViewController.swift
@@ -15,6 +15,9 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func buttonClick(_ sender: UIButton) {
+        print("This is a Git tutorial")
+    }
 
 }
 
